3a9f5ed4e5a44637629a55cd554add76fb899f0d
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2016-01-18 22:12:24 +0700

    Refactor middleware

---
+++ middleware.js
@@ -0,0 +1,13 @@
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log('private route hit');
+		next();
+	},
+	logger: function(req, res, next) {
+		var date = new Date().toString();
+		console.log('Request: ' + date + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+}
+
+module.exports = middleware;
--- server.js
+++ server.js
@@ -1,23 +1,12 @@
 var express = require('express');
 var app = express();
-
 var PORT = process.env.PORT || 3000;
+var middleware = require('./middleware');
 
-var middleware = {
-	requireAuthentication: function(req, res, next) {
-		console.log('private route hit');
-		next();
-	},
-	logger: function(req, res, next) {
-		var date = new Date().toString();
-		console.log('Request: ' + date + ' ' + req.method + ' ' + req.originalUrl);
-	}
-}
 
-app.use(middleware.requireAuthentication);
 app.use(middleware.logger);
 
-app.get('/about', function (req, res) {
+app.get('/about', middleware.requireAuthentication, function (req, res) {
 	res.send('About Us');
 });
 
