5f89a58b51a04ff80866e251f24f7cededa75080
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2016-01-23 23:37:00 +0700

    Add query ?completed filter for todos

--- server.js
+++ server.js
@@ -9,9 +9,18 @@ var todoNextId = 1; //increment unique id
 
 app.use(bodyParser.json());
 
-// GET /todos
+// GET /todos?completed=true
 app.get('/todos', function(req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	res.json(filteredTodos);
 });
 
 // GET /todos/:id
