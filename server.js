3a3adec0207ecfbfc13a6f39fe2cce5486a04d67
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2016-01-24 00:34:30 +0700

    Final static todos

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var todoNextId = 1; //increment unique id
 
 app.use(bodyParser.json());
 
-// GET /todos?completed=true
+// GET /todos?completed=true&q=work
 app.get('/todos', function(req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
@@ -19,6 +19,13 @@ app.get('/todos', function(req, res) {
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodos = _.where(filteredTodos, {completed: false});
 	}
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			// console.log(todo);
+			return todo['description'].toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1; // > -1 will return empty array if false 
+		});
+	};
 
 	res.json(filteredTodos);
 });
