9a3e4a0f67cbb9e3c756e59e5103b2f4b6d6d8f7
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2016-01-28 23:09:26 +0700

    Convert PUT /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -118,35 +118,42 @@ app.delete('/todos/:id', function(req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
+
 	// Strict only allow for expected key
 	var body = _.pick(req.body, 'description', 'completed');
 	// store key is valid to this object
-	var validAttributes = {};
+	var attributes = {};
 
-	if (!matchedTodo) {
-		return res.status(404).send();
+
+	if (body.hasOwnProperty('completed')) {
+		// Change by reference
+		attributes.completed = body.completed;
 	};
 
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		// Change by reference
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
-	}
-
-	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
+	if (body.hasOwnProperty('description')) {
+		attributes.description = body.description;
 
 		// Reference, remove bounce of space poor finger
-		validAttributes.description = validAttributes.description.trim();
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
-	}
+		attributes.description = attributes.description.trim();
+	};
 
-	//update todo use extend method
-	_.extend(matchedTodo, validAttributes);
-	res.json(matchedTodo);
+	db.todo.findById(todoId)
+		.then(function (todo) {
+			if (todo) {
+				todo.update(attributes)
+					.then(function (todo) {
+						res.json(todo);
+					},
+					function (e) {
+						res.status(400).json(e);
+					});
+			} else{
+				res.status(404).send();
+			};
+		},
+		function (e) {
+			res.status(500).send();
+		});
 });
 
 app.use(middleware.logger);
