3f725e085327bd62f2b9232e9e496b4b4f2cfa99
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2016-01-31 13:29:12 +0700

    Add hooks validate user.email to lower case and Add hash & salt password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 module.exports = function (sequelize, DataTypes) {
 	return sequelize.define('user', {
 		email: {
@@ -10,11 +13,40 @@ module.exports = function (sequelize, DataTypes) {
 				isEmail: true
 			}
 		},
+		salt: {
+			type: DataTypes.STRING
+		},
+		password_hash: {
+			type: DataTypes.STRING
+		},
 		password: {
-			type: DataTypes.STRING,
+			type: DataTypes.VIRTUAL,
 			allowNull: false,
 			validate: {
 				len: [7, 100]
+			},
+			set: function (val) {
+				var salt = bcrypt.genSaltSync(10);
+				var hash = bcrypt.hashSync(val, salt);
+
+				this.setDataValue('password', val); // Remember to set the data value, otherwise it won't be validated
+       			this.setDataValue('salt', salt);
+       			this.setDataValue('password_hash', hash);
+			}
+		}
+	}, {
+		hooks: {
+			//strict user.email to lowercase
+			beforeValidate: function (user, options) {
+				if (typeof user.email === 'string') {
+					user.email = user.email.toLowerCase();
+				};
+			}
+		},
+		instanceMethods: {
+			toPublicJSON: function () {
+				var json = this.toJSON();
+				return _.pick(json, 'id', 'email', 'updatedAt', 'createdAt');
 			}
 		}
 	});
--- server.js
+++ server.js
@@ -162,7 +162,7 @@ app.post('/users', function (req, res) {
 
 	db.user.create(body)
 		.then(function (user) {
-			res.json(user);
+			res.json(user.toPublicJSON());
 		},
 		function (e) {
 			res.status(400).json(e);
