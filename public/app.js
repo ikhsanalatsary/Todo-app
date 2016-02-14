bcf38f21eebc570956a656e4c7a8c077830129a5
ikhsanalatsary <ikhsan.alatsary@yahoo.com>
2016-02-15 01:58:04 +0700

    Add ios-dblclick directive & working with css

--- public/app.js
+++ public/app.js
@@ -318,6 +318,40 @@
 		};
 	});
 
+	app.directive('iosDblclick',
+		function () {
+
+			const DblClickInterval = 300; //milliseconds
+
+			var firstClickTime;
+			var waitingSecondClick = false;
+
+			return {
+				restrict: 'A',
+				link: function (scope, element, attrs) {
+					element.bind('click', function (e) {
+
+						if (!waitingSecondClick) {
+							firstClickTime = (new Date()).getTime();
+							waitingSecondClick = true;
+
+						setTimeout(function () {
+							waitingSecondClick = false;
+						}, DblClickInterval);
+					}
+					else {
+						waitingSecondClick = false;
+
+						var time = (new Date()).getTime();
+						if (time - firstClickTime < DblClickInterval) {
+							scope.$apply(attrs.iosDblclick);
+						}
+					}
+				});
+			}
+		};
+	});
+
 	app.filter('startFrom', function() {
 		return function(input, start) {
 			start = +start; //parse to int
